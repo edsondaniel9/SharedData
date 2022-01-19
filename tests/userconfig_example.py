84be49597f76bf8546aa8abe7a48e78571ce2292
jcarlitooliveira <jcarlitooliveira@gmail.com>
2022-01-19 18:41:14 -0300

    0.0.3

--- tests/userconfig_example.py
+++ tests/userconfig_example.py
@@ -7,6 +7,7 @@ plt.rcParams["figure.figsize"] = (10,5)
 import pandas as pd
 pd.set_option('display.float_format', lambda x: '%.4f' % x)
 
+print(__file__)
 source_directory = r'C:\src\SharedData\src'
 sys.path.insert(0,source_directory)
 
@@ -17,8 +18,12 @@ db_directory = r'C:\DB\files'
 downloads_directory = r'D:\DOWNLOADS'
 
 s3_sync = False
-s3_bucket = 's3://tradebywire/files'
-awsclipath =  r'C:\Program Files\Amazon\AWSCLIV2\aws.exe'
+s3_bucket = '[s3_bucket]'
+aws_clipath =  r'C:\Program Files\Amazon\AWSCLIV2\aws.exe'
+aws_accesskeyid='[aws_accesskeyid]'
+aws_secretkey='[aws_secretkey]'
+aws_region='[aws_region]'
+aws_outputformat='json'
 
 verbose_level = logging.INFO
 logging_level = logging.INFO
